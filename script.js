3de41a95245cf400a5221d770adb992c8a294a2a
SrKaique <100378618+kaiquemm7@users.noreply.github.com>
2025-03-08 00:26:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("surpresaBtn").addEventListener("click", function() {
+        alert("Você é muito especial para mim! Nunca se esqueça do quanto é amada e admirada! E do quanto eu sou completamente apaixonada nessa mulher linda e maravilhosa que você é. 💕");
+    });
+});
